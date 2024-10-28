d9aa896b4437ffa3963c6c120789b1272715b9d2
Steven Hall <stevenbryanb.work@gmail.com>
2024-10-28 15:56:50 +0000

    Add sln

---
+++ BearAdaptor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BearAdaptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
